b528baaf73b706cef8a6208f142cee57e3054eb5
Daniel <d.murphy@berlin-direktversicherung.de>
2023-05-10 12:52:57 +0200

    updated configs

--- index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  trailingComma: "es5",
-  tabWidth: 0,
-  semi: false,
-  singleQuote: true,
-};
---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 0,
+    semi: false,
+    singleQuote: true,
+}
